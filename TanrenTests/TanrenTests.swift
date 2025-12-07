735037d796e3df51ac219fbcc610000a07cbdf82
Anders Hovmöller <anders.hovmoller@dryft.se>
2025-12-07 10:34:13 +0100

    Initial Commit

---
+++ TanrenTests/TanrenTests.swift
@@ -0,0 +1,17 @@
+//
+//  TanrenTests.swift
+//  TanrenTests
+//
+//  Created by Anders Hovmöller on 2025-12-07.
+//
+
+import Testing
+@testable import Tanren
+
+struct TanrenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
